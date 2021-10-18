f8a1effd6e1709a7f0771f6a49e0b4d8977887d8
HONG JIE ZHANG <70243495+hongjie94@users.noreply.github.com>
2021-10-18 16:43:52 -0400

    update css url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
   
   <!-- Styles -->
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="https://firebasestorage.googleapis.com/v0/b/imagesbucket-468ae.appspot.com/o/NBA%20DATABASE%2Fapp.css?alt=media&token=3ffaa275-61e5-48fd-bfcd-1096e1b8608a">
 </head>
 <body>
   <div id="app">
