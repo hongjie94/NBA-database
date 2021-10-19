b2171b18056de5498eb8cfcc705fbcb49bdd7ddc
HONG JIE ZHANG <70243495+hongjie94@users.noreply.github.com>
2021-10-19 14:26:41 -0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,10 +19,10 @@
   <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
 
   <!-- Styles -->
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="public/css/app.css">
 
   <!-- Scripts -->
-  <script src="/js/app.js" defer></script>
+  <script src="public/js/app.js" defer></script>
   
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
