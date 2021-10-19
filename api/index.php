638f9c0bf03310492e45b440cb6f99c62a8c3d09
HONG JIE ZHANG <70243495+hongjie94@users.noreply.github.com>
2021-10-18 20:49:02 -0400

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,5 +2,5 @@
 
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
-require __DIR__ . '/../public/css/css.app';
-require __DIR__ . '/../public/js/app/js';
+require __DIR__ . '/../public/css/app.css';
+require __DIR__ . '/../public/js/app.js';
